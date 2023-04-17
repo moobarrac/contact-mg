7e6164dc0f0c544f5a9ccf3f71a1e53553bfc880
moobarrac <wahabajibola01@gmail.com>
2023-04-17 15:46:28 +0100

    added provider and query client provider component

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
+// Importing required modules
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+// Creating a new instance of QueryClient
+const queryClient = new QueryClient();
+
+// Creating a root element for the app to be rendered on
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+// Rendering the app
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // Wrapping the app in a QueryClientProvider and Provider to provide the store and query client to the app
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </QueryClientProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
